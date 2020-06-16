ce635d64f6a488dc43e525024eec0fa34200c56d
shane <sgrimes@gmail.com>
2020-06-15 17:24:52 -0700

    style cleanup to serialblinker.py

--- serialblinker.py
+++ serialblinker.py
@@ -6,16 +6,21 @@ import serial
 import time
 
 # Windows machine
-#ser = serial.Serial('COM3', 9600, parity=serial.PARITY_EVEN, timeout=5)
-ser = serial.Serial('/dev/ttyACM0', 9600, parity=serial.PARITY_EVEN, timeout=5)
+# device = 'COM3'
+# Linux machine
+# device = '/dev/ttyACM0'
+# FreeBSD
+device = '/dev/cuaU0'
+ser = serial.Serial(device, 9600, parity=serial.PARITY_EVEN, timeout=5)
 
-#output = ser.read(100)
-#print(curcolor)
+# output = ser.read(100)
+# print(curcolor)
+
 
 def setcolor(incolor):
-#    print(incolor+b'\n')
+    # print(incolor+b'\n')
     ser.write(incolor+b'\n')
-    #time.sleep(.1)
+    # time.sleep(.1)
     # This seems to be the only reliable way to clear the input buffer
     # Probably because it's a usb->serial
     ser.read_all()
@@ -23,9 +28,9 @@ def setcolor(incolor):
 
 curcolor = setcolor(b'#ff0000')
 counter = 0
-maxinwait=0
+maxinwait = 0
 while 1:
-    #time.sleep(5)
+    # time.sleep(5)
     time.sleep(.1)
     if (curcolor == b'#00ff00'):
         curcolor = setcolor(b'#ff0000')
@@ -37,11 +42,14 @@ while 1:
         setcolor(b'B#ffff00-0001#ff00ff')
     
     counter = counter + 1
-    #print('IN: {inw}, OUT: {outw}, MAXIN: {maxin} CNT: {cnt}'.format(inw=ser.in_waiting,maxin=maxinwait , outw=ser.out_waiting, cnt=counter))
+    # print('IN: {inw}, OUT: {outw}, MAXIN: {maxin} CNT: {cnt}'.format(inw=ser.in_waiting,maxin=maxinwait,
+    #                                                                  outw=ser.out_waiting, cnt=counter))
     if (ser.in_waiting > 0):
         if (ser.in_waiting > maxinwait):
             maxinwait = ser.in_waiting
 
-        print('IN: {inw}, OUT: {outw}, MAXIN: {maxin} CNT: {cnt}'.format(inw=ser.in_waiting,maxin=maxinwait , outw=ser.out_waiting, cnt=counter))
+        print('IN: {inw}, OUT: {outw}, MAXIN: {maxin} CNT: {cnt}'.format(inw=ser.in_waiting, maxin=maxinwait,
+                                                                         outw=ser.out_waiting, cnt=counter))
         print(ser.readline())
-        print('IN: {inw}, OUT: {outw}, MAXIN: {maxin} CNT: {cnt}'.format(inw=ser.in_waiting,maxin=maxinwait , outw=ser.out_waiting, cnt=counter))
+        print('IN: {inw}, OUT: {outw}, MAXIN: {maxin} CNT: {cnt}'.format(inw=ser.in_waiting, maxin=maxinwait,
+                                                                         outw=ser.out_waiting, cnt=counter))
