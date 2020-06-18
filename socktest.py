38287c36b67833e33e0b521e01c27552ee25a492
shane <sgrimes@gmail.com>
2020-06-17 22:33:45 -0700

    Initial proto for grabbing from dpinger socket

--- socktest.py
+++ socktest.py
@@ -1,11 +1,37 @@
 import socket
 import os
+import time
 
 sockpath = "/var/run/dpinger_test.sock"
 print("Connecting")
-if os.path.exists(sockpath):
-    client = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-    client.connect(sockpath)
-    print("connected")
-else:
-    print("couldn't connect")
+while True:
+    time.sleep(5)
+    if os.path.exists(sockpath):
+        client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    else:
+        print("couldn't connect")
+        raise SystemExit("whatever")
+
+    try:
+        print("Trying to connect")
+        client.connect(sockpath)
+
+        while True:
+            print("receiving...")
+            data = client.recv(32)
+            if data:
+        #        print(f'received: {}'.format(data))
+                print(len(data))
+                print(data)
+            else:
+                print("No data")
+                break
+
+    except socket.error as msg:
+        print("UhOh")
+        print(msg)
+        raise SystemExit(1)
+    finally:
+        print("closing connection")
+        client.close()
+   
