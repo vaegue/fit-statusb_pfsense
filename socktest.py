6906667c9e28a00cc287050bb364cc7b9ed0f3f6
shane <sgrimes@gmail.com>
2020-06-18 18:35:16 -0700

    increase info to console

--- socktest.py
+++ socktest.py
@@ -3,11 +3,12 @@ import os
 import time
 import glob
 
-polltime = 1
+polltime = .5
 sockpath = glob.glob('/var/run/dpinger_WAN_DHCP*.sock')
 
 # sockpath = "/var/run/dpinger_test.sock"
-print("Connecting")
+count = 0
+print("Start")
 while True:
     time.sleep(polltime)
     if os.path.exists(sockpath[0]):
@@ -17,17 +18,19 @@ while True:
         raise SystemExit("whatever")
 
     try:
-        # print("Trying to connect")
+        print("Connecting")
         client.connect(sockpath[0])
 
         while True:
-            # print("receiving...")
+            print("receiving...")
             data = client.recv(64)
             if data:
                 # {gw_name} {lat_ave} {lat_std_dev} {loss}
                 # WAN_DHCP 1168 613 0
                 # b'WAN_DHCP 1168 613 0\n'
                 result = dict(zip(('gw_name', 'lat_ave', 'stdev', 'loss'), data.decode().split()))
+                count = count+1
+                print(f'count: {count}')
                 for k in result:
                     print(f'{k}: {result[k]}')
             else:
